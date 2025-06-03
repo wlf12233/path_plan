bf816a60de908ac8b8b5720d9f5db22a00e8b32a
wlf <wulf@seer-group.com>
2025-06-03 15:57:36 +0800

    feat:astar

--- test/testAdjMatrixGraph.cpp
+++ test/testAdjMatrixGraph.cpp
@@ -3,21 +3,21 @@
 //
 #include "AdjMatrixGraph.h"
 
-int main(int argc, char *argv[]) {
-    AdjMatrixGraph g(5);
-    g.addEdge(0, 2, 5);
-    g.addEdge(1, 2, 1);
-    g.addEdge(1, 3, 2);
-    g.addEdge(2, 3, 3);
-    g.addEdge(3, 4, 1);
-    auto [dist,prev] = g.dijkstra2(0);
-    int target = 4;
-    auto path = g.reconstructPath(0,target,prev);
-    std::cout<<"2"<<std::endl;
-
-    std::cout << "Path from 0 to " << target << ": ";
-    for (int node : path) {
-        std::cout << node << " ";
-    }
-    std::cout << "\nDistance: " << dist[target] << "\n";
-}
+// int main(int argc, char *argv[]) {
+//     AdjMatrixGraph g(5);
+//     g.addEdge(0, 2, 5);
+//     g.addEdge(1, 2, 1);
+//     g.addEdge(1, 3, 2);
+//     g.addEdge(2, 3, 3);
+//     g.addEdge(3, 4, 1);
+//     auto [dist,prev] = g.dijkstra2(0);
+//     int target = 4;
+//     auto path = g.reconstructPath(0,target,prev);
+//     std::cout<<"2"<<std::endl;
+//
+//     std::cout << "Path from 0 to " << target << ": ";
+//     for (int node : path) {
+//         std::cout << node << " ";
+//     }
+//     std::cout << "\nDistance: " << dist[target] << "\n";
+// }
